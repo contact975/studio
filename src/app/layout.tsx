ce83feddfba230ecfeb522c9268a4304560fbb9c
Contact IC Accounting&Service <contact@icaccservice.com>
2026-01-24 15:10:51 +0000

    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@300;400;600;700&display=swap" rel="stylesheet" />
+        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
       </head>
       <body className={cn('font-body antialiased')}>
         {children}
