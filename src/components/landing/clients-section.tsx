4f115920f6e886c6bbadd8a4513a508f947ce86e
Contact IC Accounting&Service <contact@icaccservice.com>
2026-02-04 08:28:05 +0000

    Make the following changes:
    Find this element:
    
    Relevant files:

--- src/components/landing/clients-section.tsx
+++ src/components/landing/clients-section.tsx
@@ -32,6 +32,11 @@ const allLogos = [
     imageHint: "Customer logo 04"
   },
   {
+    name: "Customer 05",
+    logoUrl: "https://firebasestorage.googleapis.com/v0/b/studio-3153056778-cc8e4.firebasestorage.app/o/Customer05.png?alt=media",
+    imageHint: "Customer logo 05"
+  },
+  {
     name: "Ananda Development",
     logoUrl: "https://picsum.photos/seed/ananda/200/100",
     imageHint: "Ananda Development logo"
