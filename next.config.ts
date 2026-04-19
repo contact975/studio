d950da4aa3cd2ec3e69e70a2482f6ba6df158aba
Contact IC Accounting&Service <contact@icaccservice.com>
2026-04-19 17:54:52 +0000

    Edit Next.js non www to www

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,21 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'www.icaccservice.com' }],
+        destination: 'https://icaccservice.com/:path*',
+        permanent: true,
+      },
+    ];
   },
   images: {
     remotePatterns: [
